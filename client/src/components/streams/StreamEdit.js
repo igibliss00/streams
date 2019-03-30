02fc474bcda9efcd7ea84070ae20d4f435a062c4
J <socept@gmail.com>
2019-03-30 12:05:15 -0400

    Initial commit

---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StreamEdit extends React.Component {
+    render() {
+        return <div></div>
+    }
+}
+
+export default StreamEdit;
---
+++ client/src/components/streams/StreamList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StreamLists extends React.Component {
+    render() {
+        return <div></div>
+    }
+}
+
+export default StreamLists;
