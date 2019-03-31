de80b59b6ecc553036e6c7fa2944e8f9d0e8a796
J <socept@gmail.com>
2019-03-31 16:48:24 -0400

    Created stream reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import { 
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
 import streams from '../api/streams';
 
 export const signIn = userId => {
@@ -16,6 +24,35 @@ export const signOut = _ => {
 
 export const createStream = formValue => {
     return async dispatch => {
-        streams.post('/streams', formValue);
+        const response = await streams.post('/streams', formValue);
+        dispatch({type: CREATE_STREAM, payload: response.data});
     };
 }; 
+
+export const fetchStreams = _ => {
+    return async dispatch => {
+        const response = await streams.get('/streams');
+        dispatch({type: FETCH_STREAMS, payload: response.data});
+    };
+};
+
+export const fetchStream = id => {
+    return async dipatch => {
+        const response = await streams.get(`/streams/${id}`);
+        dipatch({type: FETCH_STREAM, payload: response.data});
+    };
+};
+
+export const editStream = (id, formValue) => {
+    return async dispatch => {
+        const response = await streams.edit(`/streams/${id}`, formValue);
+        dispatch({type: EDIT_STREAM, payload: response.data});
+    };
+};
+
+export const deleteStream = id => {
+    return async dispatch => {
+        await streams.delete(`/streams/${id}`);
+        dispatch({type: DELETE_STREAM, payload: id});
+    }
+}
